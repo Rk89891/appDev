a965abb0d9e488668b88a96a0137eb4398efc388
Rk89891 <85116560+Rk89891@users.noreply.github.com>
2025-01-18 11:57:01 +0800

    Create a new ReactNative app.
    Scratch.

---
+++ ProjectOneKSCompany/app/src/main/java/com/example/projectone_kscompany/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.projectone_kscompany
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.projectone_kscompany.ui.theme.ProjectOneKSCompanyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProjectOneKSCompanyTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProjectOneKSCompanyTheme {
+        Greeting("Android")
+    }
+}
